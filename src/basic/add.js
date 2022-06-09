b49514bef0d7029d54910423d7df14fa7d28923c
mDjeee <isejtniyazov@mail.ru>
2022-06-09 16:17:54 +0500

    fix: add stream into add.js

--- src/basic/add.js
+++ src/basic/add.js
@@ -1,9 +1,24 @@
 import { appendFile } from 'fs/promises';
-export default async function add(dirSet, sep, pathToFile){
+import fs from 'fs';
+export default async function add(dirSet, sep, fileName){
   try {
-    let fileName = dirSet.join(sep) + sep + pathToFile;
-    await appendFile(fileName, '', () => console.error('FS operation failed'));
-    console.log(`You are currently in ${dirSet.join(sep)}`);
+    let pathToFile = dirSet.join(sep) + sep + fileName;
+
+    let writeStream = fs.createWriteStream(pathToFile);
+
+    writeStream.write('New File', 'utf8');
+
+    writeStream.on('err', (err) => {
+      if(err){
+        console.error('Operation failed');
+      }
+    })
+
+    writeStream.on('finish', () => {
+      console.log(`You are currently in ${dirSet.join(sep)}`);
+    });
+    
+    writeStream.end();
   } catch (err) {
     // When a request is aborted - err is an AbortError
     console.error('Operation failed');
